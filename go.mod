81b53aa472df6fc26aa2ed42d936c490a670577d
Alan Tan <alanzhtan@gmail.com>
2025-03-07 10:13:06 +0000

    Go Proxy App

---
+++ go.mod
@@ -0,0 +1,23 @@
+module cloud.google.com/go/dataproc/apiv1
+
+go 1.24.0
+
+replace cloud.google.com/go => cloud.google.com/go v0.100.2
+
+require (
+	cloud.google.com/go/auth v0.15.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.7 // indirect
+	cloud.google.com/go/compute/metadata v0.6.0 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.5 // indirect
+	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/oauth2 v0.28.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/api v0.224.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250227231956-55c901821b1e // indirect
+	google.golang.org/grpc v1.71.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
+)
---
+++ proxy.go
@@ -0,0 +1,219 @@
+// authproxy proxies HTTP requests to a backend while adding auth information to every request.
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
+
+	dataprocpb "cloud.google.com/go/dataproc/apiv1/dataprocpb"
+	dataproc "cloud.google.com/go/dataproc/apiv1@v1.12.0"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option" // Import insecure credentials
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	backend             = flag.String("backend", "", "url of the backend HTTP server")
+	allowedMethods      = flag.String("allowed-methods", "GET,HEAD", "Comma-separated list of allowed HTTP methods")
+	allowedPathPrefixes = flag.String("allowed-path-prefixes", "/", "Comma-separated list of allowed HTTP request URL paths")
+	endpoint            = "" // will be defined later
+)
+
+// configHelperResp corresponds to the JSON output of the `gcloud config-helper` command.
+type configHelperResp struct {
+	Credential struct {
+		AccessToken string `json:"access_token"`
+		TokenExpiry string `json:"token_expiry"`
+	} `json:"credential"`
+}
+
+func gcloudToken() (*oauth2.Token, error) {
+	credentials, err := google.FindDefaultCredentials(context.Background())
+	if err != nil {
+		log.Fatalf("failed to get default credentials: %v", err)
+	}
+	tokenSource := credentials.TokenSource
+	if tokenSource == nil {
+		return nil, fmt.Errorf("failed to create NewTokenSource: %v", err)
+	}
+
+	// Access the token
+	token, err := tokenSource.Token()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get token: %v", err)
+	}
+	return token, nil
+}
+
+type tokenSourceFunc func() (*oauth2.Token, error)
+
+func (tsf tokenSourceFunc) Token() (*oauth2.Token, error) {
+	return tsf()
+}
+
+func defaultTokenSource(ctx context.Context) oauth2.TokenSource {
+	dts, err := google.DefaultTokenSource(ctx, "https://www.googleapis.com/auth/cloud-platform")
+	if err != nil {
+		return dts
+	}
+	return tokenSourceFunc(gcloudToken)
+}
+
+func methodAllowed(method string) bool {
+	allowed := strings.Split(*allowedMethods, ",")
+	for _, allowedMethod := range allowed {
+		if method == allowedMethod {
+			return true
+		}
+	}
+	return false
+}
+
+func pathAllowed(path string) bool {
+	prefixes := strings.Split(*allowedPathPrefixes, ",")
+	for _, allowedPrefix := range prefixes {
+		if strings.HasPrefix(path, allowedPrefix) {
+			return true
+		}
+	}
+	return false
+}
+
+// NewClusterControllerClient creates a new cluster controller client.
+func NewClusterControllerClient(ctx context.Context, opts ...option.ClientOption) (*dataproc.ClusterControllerClient, error) {
+	conn, err := grpc.Dial(endpoint, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock()) // Corrected line
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial: %v", err)
+	}
+	clusterControllerClient, err := dataproc.NewClusterControllerClient(ctx, option.WithGRPCConn(conn))
+	if err != nil {
+		return nil, err
+	}
+	return clusterControllerClient, nil // Corrected line
+}
+
+func clusterURL(ctx context.Context, project, region, clusterName string) (*url.URL, error) {
+	fullEndpoint := region + "-dataproc.googleapis.com:443"
+	// Create a new Dataproc client.
+	//define endpoint here as the region is known
+	endpoint = fullEndpoint
+	client, err := NewClusterControllerClient(ctx, option.WithEndpoint(fullEndpoint))
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	defer func(client *dataproc.ClusterControllerClient) {
+		if client != nil {
+			err := client.Close()
+			if err != nil {
+				log.Fatalf("failed to close client: %v", err)
+			}
+		}
+	}(client)
+
+	// Build the GetCluster request.
+	req := &dataprocpb.GetClusterRequest{
+		ProjectId:   project,
+		Region:      region,
+		ClusterName: clusterName,
+	}
+
+	// Call the GetCluster method.
+	cluster, err := client.GetCluster(ctx, req)
+	if err != nil {
+		log.Fatalf("failed to get cluster: %v", err)
+	}
+
+	// Check if InternalIpOnly is empty before using it
+	if cluster.Config == nil || cluster.Config.GceClusterConfig == nil {
+		return nil, fmt.Errorf("cluster %s has no GceClusterConfig", clusterName)
+	}
+
+	if !cluster.Config.GceClusterConfig.GetInternalIpOnly() {
+		return nil, fmt.Errorf("cluster %s is not configured for internal IP only", clusterName)
+	}
+
+	for _, endpoint := range cluster.Config.EndpointConfig.HttpPorts {
+		u, err := url.Parse(endpoint)
+		if err != nil {
+			return nil, err
+		}
+		u.Path = "/"
+		return u, nil
+	}
+	return nil, fmt.Errorf("no HTTP endpoints defined for the cluster %q", clusterName)
+}
+
+func targetBackendURL(r *http.Request) (*url.URL, error) {
+	if len(*backend) > 0 {
+		return url.Parse(*backend)
+	}
+
+	targetProject := os.Getenv("PROJECT")
+	targetRegion := os.Getenv("REGION")
+	targetCluster := os.Getenv("CLUSTER")
+	if len(targetCluster) > 0 {
+		return clusterURL(r.Context(), targetProject, targetRegion, targetCluster)
+	}
+	hostParts := strings.Split(r.Host, "-dot-")
+	if len(hostParts) < 2 {
+		return nil, fmt.Errorf("unable to identify cluster name from hostname")
+	}
+	return clusterURL(r.Context(), targetProject, targetRegion, hostParts[0])
+}
+
+func proxy() http.Handler {
+	tokenSource := oauth2.ReuseTokenSource(nil, defaultTokenSource(context.Background()))
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !methodAllowed(r.Method) || !pathAllowed(r.URL.Path) {
+			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+			return
+		}
+		backendURL, err := targetBackendURL(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		proxy := httputil.NewSingleHostReverseProxy(backendURL)
+		if backendURL.Scheme == "http" {
+			proxy.Transport = &http2.Transport{
+				AllowHTTP: true,
+				DialTLSContext: func(ctx context.Context, network string, addr string, cfg *tls.Config) (net.Conn, error) {
+					return net.Dial(network, addr)
+				},
+			}
+		}
+		log.Printf("[%q] proxied request for %+v: %+v", backendURL, r.URL.Path, r)
+		token, err := tokenSource.Token()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		token.SetAuthHeader(r)
+		r.Host = backendURL.Host
+		r.URL.Scheme = backendURL.Scheme
+		proxy.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	flag.Parse()
+	handler := proxy()
+	log.Printf("Starting proxy on port 8080")
+	err := http.ListenAndServe(":8080", handler)
+	if err != nil {
+		panic(err)
+	}
+}
